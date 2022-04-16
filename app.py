f4279fdea875feb1bf2fae82a2c9b803223fcceb
zhw <zhw@sfu.ca>
2022-04-16 14:41:38 -0700

    checkpoint 1

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask,request
+from flask import Flask,request,render_template,jsonify
 from requests import get
 
 app = Flask(__name__)
 SITE_NAME = 'https://discord.com/'
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+@app.route("/heartbeat")
+def heartbeat():
+    return jsonify({"status": "healthy"})
+
+@app.route('/')
+def homepage():
+	return render_template('templates/index.html')
+@app.route('/<path:path>',methods=["GET","POST"])
 def proxy(path):
-	print(path)
-	return get(f'{SITE_NAME}{path}').content
+	if request.method == "GET":
+		return (f'{SITE_NAME}{path}').content
+
+#def proxy(path):
+#	return (f'{SITE_NAME}{path}').content
 
 #if __name__ == '__main__':
 #	app.run(port=80,debug=True)
