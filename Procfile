e3806f3c9e9951700994b08e2f622b7b2f8cb821
zhw <zhw@sfu.ca>
2022-04-16 13:13:58 -0700

    change startup

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn application.py:application
+web: flask run --host=0.0.0.0 --port=80
--- app.py
+++ app.py
@@ -5,12 +5,10 @@ app = Flask(__name__)
 SITE_NAME = 'https://discord.com/'
 
 @app.route('/', defaults={'path': ''})
-
 @app.route('/<path:path>')
 def proxy(path):
 	print(path)
 	return get(f'{SITE_NAME}{path}').content
 
-if __name__ == '__main__':
-	app.run(debug=True)
-  
+#if __name__ == '__main__':
+#	app.run(port=80,debug=True)
