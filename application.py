3fdb6737f8ace5341aecdbf79cd295baa7a3b5f4
zhw <zhw@sfu.ca>
2022-04-16 12:54:31 -0700

    gunicorn

--- application.py
+++ application.py
@@ -1,15 +1,16 @@
-from flask import Flask
+from flask import Flask,request
 from requests import get
 
 app = Flask(__name__)
 SITE_NAME = 'https://discord.com/'
 
 @app.route('/', defaults={'path': ''})
+
 @app.route('/<path:path>')
 def proxy(path):
 	print(path)
 	return get(f'{SITE_NAME}{path}').content
 
 if __name__ == '__main__':
-	app.run(debug=False)
+	app.run(debug=True)
   
