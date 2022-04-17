be0378756680c440863c71035c1dda4aafe5448a
zhw <zhw@sfu.ca>
2022-04-16 17:29:50 -0700

    proxy webhook working

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
-from flask import Flask,request,render_template,jsonify
-from requests import get
+from flask import Flask,request,render_template,jsonify,Response
+from requests import post
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder="static")
 SITE_NAME = 'https://discord.com/'
 
 @app.route("/heartbeat")
 def heartbeat():
     return jsonify({"status": "healthy"})
 
-@app.route('/')
-def homepage():
-	return render_template('templates/index.html')
-@app.route('/<path:path>',methods=["GET","POST"])
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>',methods=['GET','POST'])
 def proxy(path):
 	if request.method == "GET":
-		return (f'{SITE_NAME}{path}').content
+		if path == '':
+			return render_template('index.html')
+		else:
+			return Response("<h1> 404 Not Found! </h1>",status=404)
+	elif request.method == "POST":
+		print(path)
+		data = request.get_json()
+		url = SITE_NAME + path
+		resp = post(url,data=data)
+		return Response(resp.text,status=resp.status_code)
 
 #def proxy(path):
 #	return (f'{SITE_NAME}{path}').content
