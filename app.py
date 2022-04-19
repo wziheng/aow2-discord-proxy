1f004b5197172832e3a7654b534fade48d8327a5
zhw <zhw@sfu.ca>
2022-04-18 17:17:54 -0700

    add rate limit to prevent too many requests

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask,request,render_template,jsonify,Response
+from numpy import number
 from requests import post
+from time import time
 
 app = Flask(__name__,static_folder="static")
 SITE_NAME = 'https://discord.com/'
+CAN_SEND = {}
 
 @app.route("/heartbeat")
 def heartbeat():
@@ -17,14 +20,18 @@ def proxy(path):
 		else:
 			return Response("<h1> 404 Not Found! </h1>",status=404)
 	elif request.method == "POST":
-		print(path)
-		data = request.get_json()
-		url = SITE_NAME + path
-		resp = post(url,data=data)
-		return Response(resp.text,status=resp.status_code)
-
-#def proxy(path):
-#	return (f'{SITE_NAME}{path}').content
-
-#if __name__ == '__main__':
-#	app.run(port=80,debug=True)
+		channel_id = path[13:31]
+		if channel_id in CAN_SEND:
+			print(time()-CAN_SEND[channel_id])
+		if not channel_id in CAN_SEND or time() >= CAN_SEND[channel_id]:
+			data = request.get_json()
+			url = SITE_NAME + path
+			resp = post(url,data=data)
+			#useful_headers = ["X-RateLimit-Limit","X-RateLimit-Remaining","X-RateLimit-Reset","X-RateLimit-Reset-After","X-RateLimit-Bucket"]
+			#for header in useful_headers:
+			#	print(header,resp.headers[header])
+			if resp.status_code == 429 or int(resp.headers["X-RateLimit-Remaining"]) == 0:
+				CAN_SEND[channel_id] = float(resp.headers["X-RateLimit-Reset"])
+			return Response(resp.text,status=resp.status_code)
+		else:
+			return Response("403 Forbidden (Requests Exceeded)",status=403)
